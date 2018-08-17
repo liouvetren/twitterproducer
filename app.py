ed9a1027bfbd9f754c682a6bee00c86f4b4ebd48
shengs <shengs@149-162-137-191.dhcp-in.iupui.edu>
2018-08-17 14:29:15 -0400

    update

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ class TwitterProducer(object):
 				for message in f:
 					self.producer.produce(topic, value=message)
 					num += 1
-					sleeptime = 0.001*(1 + sin(time()*pi/30.)) + random() * 0.0002
+					sleeptime = 0.001*(1 + sin(time()*pi/120.)) + random() * 0.0002
 					sleep(sleeptime)
 			st += dt
 			print ( "%f time passed" % (time()-producer_start,) )
